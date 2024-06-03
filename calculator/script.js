268ee2c0aff417a6faaae0f4048aca2f08bafe8d
Hritik sharma <146111768+hritik2004-cse@users.noreply.github.com>
2024-06-03 17:18:36 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,7 @@
+function changeColor(color) {
+    document.documentElement.style.setProperty('--font-col', color);
+}
+
+function changeTheme(theme){
+    document.documentElement.style.setProperty("--body-col" ,"white")
+}
